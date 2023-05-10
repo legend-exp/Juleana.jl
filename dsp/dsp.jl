902f5442bbf0e12c201d1dd0c16739aab5e50840
Florian Henkes <florian.henkes@outlook.com>
2023-05-10 12:05:04 +0200

    implemented dsp

--- dsp/dsp.jl
+++ dsp/dsp.jl
@@ -3,17 +3,13 @@ include("../utils/loader.jl")
 include("../utils/utils.jl")
 
 # global variables
-enc_pickoff = 32u"µs"
 bl_mean_min, bl_mean_max = 0u"µs", 39u"µs"
 pz_fit_min, pz_fit_max = 80u"µs", 110u"µs"
 t0_threshold = 5.0
-# zac_filter_length = 30u"µs"
 
-e_grid_rt = 7u"µs":0.5u"µs":12u"µs"
-e_grid_ft = 1u"µs":0.2u"µs":4u"µs"
 
 # function processChannel(wvfs_ch::RDWaveform, bl_fc::Vector, ts_ch::Array, evID_ch::Array, ch_ch::Array, efc_ch::Array, out_t::TypedTables.Table, τ::Float32)
-function processChannel(wvfs_ch, bl_fc, ts_ch, evID_ch, ch_ch, efc_ch, out_t::TypedTables.Table, τ)
+function processChannel(wvfs_ch, bl_fc, ts_ch, evID_ch, efc_ch, τ, rt, ft)
     # get baseline mean, std and slope
     bl_stats = signalstats.(wvfs_ch, bl_mean_min, bl_mean_max)
 
@@ -23,18 +19,17 @@ function processChannel(wvfs_ch, bl_fc, ts_ch, evID_ch, ch_ch, efc_ch, out_t::Ty
     # substract baseline from waveforms
     wvfs_ch = shift_waveform.(wvfs_ch, -bl_stats.mean)
 
+    # extract decay times
+    decay_times_extracted = tailstats.(wvfs_ch, pz_fit_min, pz_fit_max)
+
     # deconvolute waveform
     deconv_flt = InvCRFilter(τ)
     wvfs_ch_pz = deconv_flt.(wvfs_ch)
-
-    # extract decay times
-    decay_times_extracted = tailstats.(wvfs_ch, pz_fit_min, pz_fit_max)
 
     # t0 determination
     # filter with fast asymetric trapezoidal filter and truncate waveform
     uflt_asy_t0 = TrapezoidalChargeFilter(40u"ns", 100u"ns", 2000u"ns")
     uflt_trunc_t0 = TruncateFilter(0u"µs"..60u"µs")
-
 
     # eventuell zwei schritte!!!
     wvfs_flt_asy_t0 = uflt_asy_t0.(uflt_trunc_t0.(wvfs_ch_pz))
@@ -70,208 +65,101 @@ function processChannel(wvfs_ch, bl_fc, ts_ch, evID_ch, ch_ch, efc_ch, out_t::Ty
 
     wvfs_flt_10410 = uflt_10410.(wvfs_ch_pz)
     e_10410        = SignalEstimator(PolynomialDNI(3, 100u"ns")).(wvfs_flt_10410, t0 .+ 12u"µs")
-    enc_10410      = SignalEstimator(PolynomialDNI(2, 150u"ns")).(wvfs_flt_10410, enc_pickoff)
 
     uflt_zac10410 = ZACChargeFilter(10u"µs", 4u"µs", 30u"µs")
 
     wvfs_flt_zac10410 = uflt_zac10410.(wvfs_ch_pz)
     e_zac_10410       = SignalEstimator(PolynomialDNI(3, 100u"ns")).(wvfs_flt_zac10410, t0 .+ 12u"µs")
-    enc_zac_10410     = SignalEstimator(PolynomialDNI(2, 150u"ns")).(wvfs_flt_zac10410, enc_pickoff)
 
 
-    # get energy grid for efficient optimization
-    e_grid   = zeros(Float32, length(e_grid_ft), length(e_grid_rt), length(wvfs_ch_pz))
-    enc_grid = zeros(Float32, length(e_grid_ft), length(e_grid_rt), length(wvfs_ch_pz))
-    for (f, ft) in enumerate(e_grid_ft)
-        for (r, rt) in enumerate(e_grid_rt)
-            if rt < ft
-                continue
-            end
-            uflt_rtft      = TrapezoidalChargeFilter(rt, ft)
-            
-            wvfs_flt_rtft  = uflt_rtft.(wvfs_ch_pz)
+    # get energy of optimized rise and flat-top time
+    uflt_rtft = TrapezoidalChargeFilter(rt, ft)
 
-            e_rtft         = SignalEstimator(PolynomialDNI(4, 80u"ns")).(wvfs_flt_rtft, t0 .+ (rt + ft/2))
-            enc_rtft       = SignalEstimator(PolynomialDNI(4, 80u"ns")).(wvfs_flt_rtft, enc_pickoff)
+    wvfs_flt_rtft  = uflt_rtft.(wvfs_ch_pz)
+    e_rtft         = SignalEstimator(PolynomialDNI(3, 100u"ns")).(wvfs_flt_rtft, t0 .+ (rt + ft/2))
 
-            e_grid[f, r, :]     = e_rtft
-            enc_grid[f, r, :]   = enc_rtft
-        end
-    end
-
-    e_zac_grid   = zeros(Float32, length(e_grid_ft), length(e_grid_rt), length(wvfs_ch_pz))
-    enc_zac_grid = zeros(Float32, length(e_grid_ft), length(e_grid_rt), length(wvfs_ch_pz))
-    for (f, ft) in enumerate(e_grid_ft)
-        for (r, rt) in enumerate(e_grid_rt)
-            if rt < ft
-                continue
-            end
-            uflt_zac_rtft      = ZACChargeFilter(rt, ft, 30.0u"µs")
-            
-            wvfs_zac_flt_rtft  = uflt_zac_rtft.(wvfs_ch_pz)
-
-            e_zac_rtft         = SignalEstimator(PolynomialDNI(4, 80u"ns")).(wvfs_zac_flt_rtft, t0 .+ (rt + ft/2))
-            enc_zac_rtft       = SignalEstimator(PolynomialDNI(4, 80u"ns")).(wvfs_zac_flt_rtft, enc_pickoff)
-
-            e_zac_grid[f, r, :]     = e_zac_rtft
-            enc_zac_grid[f, r, :]   = enc_zac_rtft
-        end
-    end
 
     # extract current with filter length of 180ns with second order polynominal and first derivative
     sgflt_deriv = SavitzkyGolayFilter(180u"ns", 2, 1)
     wvfs_sgflt_deriv = sgflt_deriv.(wvfs_ch_pz)
     current_max = maximum.(wvfs_sgflt_deriv.signal)
 
-    # save all data
-    append!(out_t.channel, ch_ch)
-
-
-    append!(out_t.blmean, bl_stats.mean)
-    append!(out_t.blsigma, bl_stats.sigma)
-    append!(out_t.blslope, bl_stats.slope)
-    append!(out_t.bloffset, bl_stats.offset)
-    
-    append!(out_t.τ_extracted, decay_times_extracted)
-    append!(out_t.τ, zeros(length(ch_ch))u"µs" .+ τ)
-    append!(out_t.t0, t0)
-    
-    append!(out_t.e_10410, e_10410)
-    append!(out_t.enc_10410, enc_10410)
-
-    append!(out_t.e_zac_10410, e_zac_10410)
-    append!(out_t.enc_zac_10410, enc_zac_10410)
-
-    append!(out_t.qdrift, qdrift)
-
-    for arr in VectorOfSimilarArrays(e_grid)
-        push!(out_t.e_grid, arr)
-    end
-
-    for arr in VectorOfSimilarArrays(enc_grid)
-        push!(out_t.enc_grid, arr)
-    end
-
-    for arr in VectorOfSimilarArrays(e_zac_grid)
-        push!(out_t.e_zac_grid, arr)
-    end
-
-    for arr in VectorOfSimilarArrays(enc_zac_grid)
-        push!(out_t.enc_zac_grid, arr)
-    end
-
-    append!(out_t.a, current_max)
-
-    append!(out_t.blfc, bl_fc)
-    append!(out_t.timestamp, ts_ch)
-    append!(out_t.eventID_fadc, evID_ch)
-    append!(out_t.e_fc, efc_ch)
-
-    append!(out_t.pretrace_diff, pretrace_diff)
-
-    append!(out_t.rt1090, rt1090)
-    append!(out_t.rt1099, rt1099)
-    append!(out_t.rt9099, rt9099)
-    append!(out_t.drift_time, drift_time)
-
-    return 1
+    # output Table 
+    return TypedTables.Table(blmean = bl_stats.mean, blsigma = bl_stats.sigma, blslope = bl_stats.slope, bloffset = bl_stats.offset, 
+    t0 = t0, τ = decay_times_extracted,
+    e_10410 = e_10410, e_zac_10410 = e_zac_10410,
+    e_trap = e_rtft,
+    qdrift = qdrift,
+    a = current_max,
+    blfc = bl_fc, timestamp = ts_ch, eventID_fadc = evID_ch, e_fc = efc_ch,
+    pretrace_diff = pretrace_diff, 
+    rt1090 = rt1090, rt1099 = rt1099, rt9099 = rt9099, drift_time = drift_time
+    )
 end
 
-function processFile(filename::PosixPath, outfilename::PosixPath, decay_times::Dict{Int, Float32})
-    data = LHDataStore(string(filename))["ORFlashCamADCWaveform"][:]
-    out_data = LHDataStore(string(outfilename), "cw")
+function processFile(filename::String, outfilename::String, channel_list::Array{String}, decay_times::Dict{String, Any}, trap_rt::Dict{String, Any}, trap_ft::Dict{String, Any})
+    data = LHDataStore(filename)
+    out_data = LHDataStore(outfilename, "cw")
 
-    if "DSP" in keys(out_data)
-        println(format("Output file {} already exists, will skip", outfile))
-        close(out_data)
-        return 0
-    end
+    for ch in channel_list
+        if ch in keys(out_data)
+            println(format("Channel {} already processed, will skip", ch))
+            continue
+        end
+        println("Processing channel $ch")
+        data_ch = data[format("{}/raw", ch)][:]
+        
+        wvfs_ch      = data_ch.waveform
+        blfc_ch      = data_ch.baseline
+        ts_ch        = data_ch.timestamp
+        evID_ch      = data_ch.eventnumber
+        efc_ch       = data_ch.daqenergy
 
-    channels     = 6* data.card + data.ch_orca
-    wvfs         = data.waveform
-    baselines_fc = data.baseline
-    timestamps   = data.timestamp
-    eventID_fadc = data.eventnumber
-    energy_fadc  = data.daqenergy
+        println("Loaded data")
+        println("Number of events: ", length(wvfs_ch))
 
-    println("Loaded data")
-    println("Number of events: ", length(wvfs))
-
-    # split waveforms channelwise
-    sortindices      = sortperm(channels, alg=ThreadsX.QuickSort)
-
-    channels_sorted     = channels[sortindices]
-    wvfs_sorted         = wvfs[sortindices]
-    baselines_fc_sorted = baselines_fc[sortindices]
-    timestamps_sorted   = timestamps[sortindices]
-    eventID_fadc_sorted = eventID_fadc[sortindices]
-    energy_fadc_sorted  = energy_fadc[sortindices]
-    
-    con_ch_view    = consgroupedview(channels_sorted, channels_sorted)
-    con_wvf_view   = consgroupedview(channels_sorted, wvfs_sorted)
-    con_bl_fc_view = consgroupedview(channels_sorted, baselines_fc_sorted)
-    con_ts_view    = consgroupedview(channels_sorted, timestamps_sorted)
-    con_evID_view  = consgroupedview(channels_sorted, eventID_fadc_sorted)
-    con_e_fc_view  = consgroupedview(channels_sorted, energy_fadc_sorted)
-
-    # output Table 
-    out_t = TypedTables.Table(channel=Int[], blmean = Float64[], blsigma = Float64[], blslope = Float64[]u"ns^-1", bloffset = Float64[], 
-    τ_extracted = Float64[]u"µs", τ = Float64[]u"µs", t0 = Float64[]u"µs",
-    e_10410 = Float64[], enc_10410 = Float64[], e_zac_10410 = Float64[], enc_zac_10410 = Float64[],
-    qdrift = Float64[], 
-    e_grid = VectorOfSimilarArrays(ElasticArray{Float32}(undef, length(e_grid_ft), length(e_grid_rt), 0)), enc_grid = VectorOfSimilarArrays(ElasticArray{Float32}(undef, length(e_grid_ft), length(e_grid_rt), 0)),
-    e_zac_grid = VectorOfSimilarArrays(ElasticArray{Float32}(undef, length(e_grid_ft), length(e_grid_rt), 0)), enc_zac_grid = VectorOfSimilarArrays(ElasticArray{Float32}(undef, length(e_grid_ft), length(e_grid_rt), 0)),
-    a = Float64[],
-    blfc = Float64[], timestamp = Float64[]u"s", eventID_fadc = Int[], e_fc = Float64[],
-    pretrace_diff = Float64[], 
-    rt1090 = Float64[]u"ns", rt1099 = Float64[]u"ns", rt9099 = Float64[]u"ns", drift_time = Float64[]u"ns"
-    )
-
-    for (i, wvfs_ch) in enumerate(con_wvf_view)
-        blfc_ch = con_bl_fc_view[i]
-        ts_ch   = con_ts_view[i]
-        evID_ch = con_evID_view[i]
-        ch_ch   = con_ch_view[i]
-        efc_ch  = con_e_fc_view[i]
-
-        ch = ch_ch[1]
-
-        # println("\nChannel: ", ch)
-        # println("Number of events: ", length(wvfs_ch))
         if length(wvfs_ch) < 2
-            # println("Skip channel")
             continue
         end
         if !haskey(decay_times, ch)
-            # println("Skip channel")
             continue
         end
 
-        processChannel(wvfs_ch, blfc_ch, ts_ch, evID_ch, ch_ch, efc_ch, out_t, decay_times[ch]u"µs")
+        out_data[ch] = processChannel(wvfs_ch, blfc_ch, ts_ch, evID_ch, efc_ch, decay_times[ch], trap_rt[ch], trap_ft[ch])
 
     end
     
 
-    # write data to HDF5
-    out_data["DSP"] = out_t
+    # close data file
     close(out_data)
 
-    printfmtln("Finished processing file: {}", FilePathsBase.filename(filename))
+    printfmtln("Finished processing file: {}", FilePathsBase.filename(PosixPath(filename)))
 
     return 1
 end
 
 
-function dsp(config_folder::String, period::Int, run::Int, preName::String, cal::Bool)
+function dsp(config_folder::PosixPath, period::Int, calrun::Int, experiment::String, is_cal::Bool)
     println("Starting DSP\n\n")
     printfmtln("Using {} threads\n\n", Threads.nthreads())
 
-    data_folder, out_data_folder, string_numbers, decay_times = prepareDSP(config_folder, period=period, run=run, preName=preName, cal=cal)
+    channel_list, label_dict, label_list_ext, string_dict, folder_dict = loadMeta(config_folder, period=period, run=calrun, experiment=experiment, cal=is_cal)
 
-    println()
-    println()
+    channel_label_dict = Dict{String, String}(values(label_dict) .=> keys(label_dict))
 
-    for (root, dirs, files) in walkdir(data_folder)
+    decay_times = loadValues(collect(values(label_dict)), "tau", config_folder, period=period, run=calrun, experiment=experiment, cal=is_cal)
+    decay_times = Dict{String, Any}([channel_label_dict[k] for k in keys(decay_times)] .=> values(decay_times).*1u"µs")
+
+    trap_rt     = loadValues(collect(values(label_dict)), "trap_rt", config_folder, period=period, run=calrun, experiment=experiment, cal=is_cal)
+    trap_rt     = Dict{String, Any}([channel_label_dict[k] for k in keys(trap_rt)] .=> values(trap_rt).*1u"µs")
+
+    trap_ft     = loadValues(collect(values(label_dict)), "trap_ft", config_folder, period=period, run=calrun, experiment=experiment, cal=is_cal)
+    trap_ft     = Dict{String, Any}([channel_label_dict[k] for k in keys(trap_ft)] .=> values(trap_ft).*1u"µs")
+
+    folder_raw, folder_dsp = folder_dict["folder_raw"], folder_dict["folder_dsp"]
+    checkFolder(PosixPath(folder_dsp), true)
+
+    for (root, dirs, files) in walkdir(folder_raw)
         iter = ProgressBar(files)
         for data_file in iter
             
@@ -280,12 +168,12 @@ function dsp(config_folder::String, period::Int, run::Int, preName::String, cal:
                 continue
             end
 
-            filename = joinpath(data_folder, data_file)
+            filename = joinpath(folder_raw, data_file)
             
-            outfile = string("dsp_", split(data_file, "_")[2])
-            outfilename = joinpath(out_data_folder, outfile)
+            outfile = string(split(data_file, "_")[1], "_dsp.lh5")
+            outfilename = joinpath(folder_dsp, outfile)
 
-            processFile(filename, outfilename, decay_times)
+            processFile(filename, outfilename, channel_list, decay_times, trap_rt, trap_ft)
             break
         end
     end
@@ -293,5 +181,9 @@ function dsp(config_folder::String, period::Int, run::Int, preName::String, cal:
 end
 
 # test DSP
-config_folder = "/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts/configs/"
-dsp(config_folder, 1, 25, "l60", true)
+# is_cal = true
+# period = 2
+# calrun = 6
+# config_folder = p"/home/iwsatlas1/henkes/l200/l200-p02-analysis/configs/"
+# experiment = "l200"
+# dsp(config_folder, period, calrun, experiment, is_cal)
--- dsp/parallel.jl
+++ dsp/parallel.jl
@@ -1,27 +1,33 @@
 using Distributed
 using ProgressMeter
 
-include("dsp.jl")
+include("../utils/packages.jl")
 include("../utils/loader.jl")
+include("../utils/utils.jl")
+
+include("dsp.jl")
+
 
 # Define the server addresses
 cslg1, cslg2, cslg3             = ["henkes@cslg-01.mpp.mpg.de"], ["henkes@cslg-02.mpp.mpg.de"], ["henkes@cslg-03.mpp.mpg.de"]
 tristanserv01, tristanserv02    = ["henkes@tristanserv01.mpp.mpg.de"], ["henkes@tristanserv02.mpp.mpg.de"]
 
 # CSLG01: 56 threads = 28 cores
-addprocs(cslg1, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 14 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
-addprocs(cslg1, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 14 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
+# addprocs(cslg1, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 14 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
+# addprocs(cslg1, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 14 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
 
 # CSLG02: 72 threads = 36 cores
 addprocs(cslg2, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 18 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
 addprocs(cslg2, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 18 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
 
 # CSLG03: 128 threads = 64 cores
-addprocs(cslg3, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 32 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
-addprocs(cslg3, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 32 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
+addprocs(cslg3, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 16 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
+addprocs(cslg3, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 16 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
+addprocs(cslg3, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 16 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
+addprocs(cslg3, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 16 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
 
 # CSLG04: 128 threads = 64 cores, create 2 workers on the localhost
-addprocs(4, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 32 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, topology=:master_worker)
+addprocs(4, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 16 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, topology=:master_worker)
 
 # # Tristanserv01: 56 threads = 28 cores
 # addprocs(tristanserv01, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 14 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
@@ -32,32 +38,57 @@ addprocs(4, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t
 # addprocs(tristanserv02, exename=`/home/iwsatlas1/henkes/julia-1.8.5/bin/julia`, exeflags=`-t 18 --project=/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts`, sshflags=`-i /home/iwsatlas1/henkes/.ssh/id_mpp`, max_parallel=4, topology=:master_worker)
 
 
-# Split the execution into two threads, each using 64 cores
 @everywhere begin
     using Distributed
     # using ProgressMeter
-    include(joinpath(@__DIR__,"dsp.jl"))
+    include(joinpath(@__DIR__,"../utils/packages.jl"))
     include(joinpath(@__DIR__,"../utils/loader.jl"))
+    include(joinpath(@__DIR__,"../utils/utils.jl"))
 
-    config_folder = "/home/iwsatlas1/henkes/legend/julia/legend-julia-dsp-scripts/configs/"
+    include(joinpath(@__DIR__,"dsp.jl"))
+end
 
-    period, run, preName, cal = 1, 25, "l60", true
-    data_folder, out_data_folder, string_numbers, decay_times = prepareDSP(config_folder, period=period, run=run, preName=preName, cal=cal)
+@everywhere begin
+    is_cal = true
+    period = 2
+    calrun = 11
+    config_folder = p"/home/iwsatlas1/henkes/l200/l200-p02-analysis/configs/"
+    experiment = "l200"
+
+    channel_list, label_dict, label_list_ext, string_dict, folder_dict = loadMeta(config_folder, period=period, run=calrun, experiment=experiment, cal=is_cal)
+
+    channel_label_dict = Dict{String, String}(values(label_dict) .=> keys(label_dict))
+
+    decay_times = loadValues(collect(values(label_dict)), "tau", config_folder, period=period, run=calrun, experiment=experiment, cal=is_cal)
+    decay_times = Dict{String, Any}([channel_label_dict[k] for k in keys(decay_times)] .=> values(decay_times).*1u"µs")
+
+    trap_rt     = loadValues(collect(values(label_dict)), "trap_rt", config_folder, period=period, run=calrun, experiment=experiment, cal=is_cal)
+    trap_rt     = Dict{String, Any}([channel_label_dict[k] for k in keys(trap_rt)] .=> values(trap_rt).*1u"µs")
+
+    trap_ft     = loadValues(collect(values(label_dict)), "trap_ft", config_folder, period=period, run=calrun, experiment=experiment, cal=is_cal)
+    trap_ft     = Dict{String, Any}([channel_label_dict[k] for k in keys(trap_ft)] .=> values(trap_ft).*1u"µs")
+
+    folder_raw, folder_dsp = folder_dict["folder_raw"], folder_dict["folder_dsp"]
 end
 println("Starting DSP\n\n")
 printfmtln("Using {} threads on PID 1\n\n", Threads.nthreads())
-
-
-for (root, dirs, files) in walkdir(data_folder)
+println()
+checkFolder(PosixPath(folder_dsp), true)
+println("Start DSP for $experiment, period $period, run $calrun")
+println("Loading meta data")
+println("Using folder $folder_raw for raw data")
+println("Using folder $folder_dsp for dsp data")
+println()
+for (root, dirs, files) in walkdir(folder_raw)
     hdf5_files = filter(f -> endswith(f, ".lh5"), files)
     status = @showprogress pmap(hdf5_files) do data_file
         try
-            filename = joinpath(data_folder, data_file)
+            filename = joinpath(folder_raw, data_file)
             
-            outfile = string("dsp_", split(data_file, "_")[2])
-            outfilename = joinpath(out_data_folder, outfile)
+            outfile = string(split(data_file, "_")[1], "_dsp.lh5")
+            outfilename = joinpath(folder_dsp, outfile)
 
-            processFile(filename, outfilename, decay_times)
+            processFile(filename, outfilename, channel_list, decay_times, trap_rt, trap_ft)
             true # success
         catch e
             false # failure
