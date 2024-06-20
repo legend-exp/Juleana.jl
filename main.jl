23ef0c22ad791873df3a822427bd5a59cba8ac6d
LisaSchlueter <lschlueter@lbl.gov>
2024-06-21 01:19:44 +0200

    changed start line
    git branch

--- main.jl
+++ main.jl
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S julia -t 1 --project=/ptmp/fhenkes/l200/current/jlenv --heap-size-hint=10G
+# julia -t 1 --project=/ptmp/lschl/l200/current/jlenv --heap-size-hint=10G main.jl --config ./config/processing_config.json -p 3 -r 0 1 --only_runs
 
 # set julia traget to generic for similar compilecache in all workers
 ENV["JULIA_CPU_TARGET"] = "generic;sandybridge,-xsaveopt,clone_all;haswell,-rdrnd,base(1)"
