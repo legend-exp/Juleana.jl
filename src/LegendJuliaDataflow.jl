d3c05148aefaaec4376ab117d1d2fd4b38a77590
Florian Henkes <florian.henkes@outlook.com>
2024-07-23 22:44:46 +0200

    Load config first

--- src/LegendJuliaDataflow.jl
+++ src/LegendJuliaDataflow.jl
@@ -6,11 +6,11 @@ include("startup.jl")
 # startup functions with command line utilities
 include("config.jl")
 
-# Utilities and functionalities for parallel processing
-include("parallel.jl")
-
 # Log text for reports
 include("log_texts.jl")
+
+# Utilities and functionalities for parallel processing
+include("parallel.jl")
 
 # SLURM job submission utilities
 include("slurm.jl")
