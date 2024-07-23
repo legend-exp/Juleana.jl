a0ac6f7f52f04fbd820eff7ea7e3f38f91c7ed0d
Florian Henkes <florian.henkes@outlook.com>
2024-07-23 22:43:17 +0200

    New main layout --> Attention: still WIP

--- main.jl
+++ main.jl
@@ -1,14 +1,22 @@
-#!/usr/bin/env -S julia -t 1 --project=/ptmp/fhenkes/l200/current/jlenv --heap-size-hint=10G
+#!/usr/bin/env -S julia -t 8 --heap-size-hint=10G
 
 ##################
 # Start Processing
 ##################
 include(joinpath(@__DIR__,"src/juleana.jl"))
 
-# load utils
-import Pkg
+# load default packages
+import Pkg, REPL
+using REPL.TerminalMenus
+
+# check if environment is default environment, than activate project
 @info "Using Julia $VERSION"
-@info "Using Julia project $(dirname(Pkg.project().path))"
+if last(split(string(dirname(Pkg.project().path)), ".julia/")) == last(split(string(joinpath(first(Pkg.DEPOT_PATH), "environments", "v$(VERSION.major).$(VERSION.minor)")), ".julia/"))
+    @info "Using dataflow project"
+    Pkg.activate(@__DIR__)
+else
+    @info "Using Julia project $(dirname(Pkg.project().path))"
+end
 @info "Load LEGEND packages"
 
 # precompile all packages
@@ -18,7 +26,7 @@ Pkg.precompile()
 include(joinpath(@__DIR__,"src/LegendJuliaDataflow.jl"))
 
 # evaluate config
-l200, processing_config, runs, periods, partitions = get_processingconfig()
+l200, processing_config, runs, periods = get_processingconfig()
 @info "Start Data processing"
 
 # load all available processors
@@ -29,7 +37,7 @@ runmode = SlurmRun(
     slurm_flags = get_slurm_flags(processing_config),
     julia_flags = get_julia_flags(processing_config),
     redirect_output = false,
-    # env = processing_config.env_args_worker
+    # env = first(processing_config.env_args_worker)
 )
 @info "Write worker start script to $(joinpath(@__DIR__, "startjlworkers.sh"))"
 write_worker_start_script(joinpath(@__DIR__, "startjlworkers.sh"), runmode)
@@ -42,97 +50,273 @@ if processing_config.submit_slurm
     @async runworkers(runmode)
 end
 
+# set up dependency graph
+using ConcurrentCollections
+possible_steps = filter!(x -> x != :default, Symbol.(keys(processing_config.processors)))
+# p_status = Dict{Symbol, Bool}(possible_steps .=> .!getproperty.(getproperty.(Ref(processing_config.processors), possible_steps), Ref(:enabled)))
+process_status = ConcurrentDict{DataPeriod, ConcurrentDict{Symbol, ConcurrentDict{DataRun, Bool}}}()
+for period in periods
+    # get all processable runs
+    processable_runs = get_proccessable_runs(runs, period)
+    # set up process status for each period
+    process_status[period] = ConcurrentDict{Symbol, ConcurrentDict{DataRun, Bool}}(possible_steps .=> [ConcurrentDict{DataRun, Bool}(processable_runs .=> Ref(p)) for p in .!getproperty.(getproperty.(Ref(processing_config.processors), possible_steps), Ref(:enabled))])
+end
+
+possible_p_steps = filter!(x -> x != :default, Symbol.(keys(processing_config.p_processors)))
+
+p_process_status = ConcurrentDict{DataPeriod, ConcurrentDict{Symbol, Bool}}()
+for period in periods
+    p_process_status[period] = ConcurrentDict{Symbol, Bool}(possible_p_steps .=> .!getproperty.(getproperty.(Ref(processing_config.p_processors), possible_p_steps), Ref(:enabled)))
+end
+
+# has_dependencies(processing_config, process, period)
+# period = periods[1]
+# process = :process_dsp_cal
+# dependencies = Symbol.(processing_config.processors[process].dependencies)
+# dep = dependencies[1]
+# all([p_process_status[period][dep] for dep in dependencies])
+
+
 flush(stdout)
-using REPL
-term = REPL.Terminals.TTYTerminal("dumb", stdin, stdout, stderr)
-repl = REPL.LineEditREPL(term, true)
-REPL.run_repl(repl)
 
-####################
-# Process Runs
-####################
-if !processing_config.only_partitions
+#################
+# Interactive Mode
+#################
+if isinteractive()
     
-    # get processing steps from config and sort by rank
-    process_steps =  processing_config.process_steps
+    function menu()        
+        # main menu options
+        options = ["Reload processors", "Submit workers", "Execute processors", "Exit"]
+        # main menu
+        menu = RadioMenu(options)
+        choice = request("Select action:", menu)
+        
+        # reload all processors from files
+        if choice == 1
+            include.(filter(contains(r".jl$"), readdir(joinpath(@__DIR__, "processors/"); join=true)))
+        # add workers according to slurm settings
+        elseif choice == 2
+            @async runworkers(runmode)
+        # execute processing steps
+        elseif choice == 3
+            # create menus for processing steps
+            steps_menu = MultiSelectMenu(String.(processing_config.possible_process_steps))
+            p_steps_menu = MultiSelectMenu(String.(processing_config.p_possible_process_steps))
+            
+            # processing steps menu to select and deselect
+            process_steps = processing_config.possible_process_steps[collect(request("Select processing steps to be executed:", steps_menu))]
+            process_steps = sort(process_steps, by = s -> processing_config.processors[s].rank)
+            p_process_steps = processing_config.p_possible_process_steps[collect(request("Select partition processing steps to be executed:", p_steps_menu))]
+            p_process_steps = sort(p_process_steps, by = s -> processing_config.p_processors[s].rank)
 
-    # process periods
-    @sync begin
-        for period in periods
-            Threads.@spawn begin
-                @info "Process period $period"
+            # execute steps one after each other without period and run parallelization
+            if isempty(process_steps) && isempty(p_process_steps)
+                @warn "No processing steps selected"
+                return
+            else
+                @sync begin
 
-                # select runs to process
-                processable_runs = get_proccessable_runs(runs, period)
-
-                # process runs
-                for run in processable_runs
-                    # check if run is a analysis run if switched on
-                    if processing_config.analysis_runs_only && !is_analysis_phy_run(l200, period, run)
-                        @warn "Run $run is not a analysis run"
-                        continue
-                    end
-
-                    # submit slurm job if enabled
-                    if processing_config.submit_slurm
-                        sbatch(l200, processing_config, period, run)
-                        continue
-                    end
-
+                    # process periods
                     Threads.@spawn begin
-                        # iterate through process steps
-                        @info "Process run $run"
-                        for process in process_steps
-                            @info "$process"
-                            flush(stdout)
+                        for period in periods
+                            Threads.@spawn begin
+                                @info "Process period $period"
 
-                            # run process
-                            kwargs = NamedTuple([(k, v) for (k, v) in pairs(processing_config.processors[process]) if !(k in [:enabled, :rank, :n_workers])])
-                            getfield(Main, process)(processing_config, l200, period, run,; kwargs...)
-                            flush(stdout)
+                                # select runs to process
+                                processable_runs = sort(collect(keys(last(first(process_status[period])))))
+
+                                # process runs
+                                for run in processable_runs
+                                    # check if run is a analysis run if switched on
+                                    if processing_config.analysis_runs_only && !is_lrun(l200, (period, run, :cal))
+                                        @warn "Run $run is not a analysis run"
+                                        continue
+                                    end
+
+                                    Threads.@spawn begin
+                                        # iterate through process steps
+                                        @info "Process run $run"
+                                        for process in process_steps
+                                            @info "$process"
+                                            flush(stdout)
+                                            
+                                            # check if process has dependencies
+                                            if !processing_config.only_runs
+                                                # get all dependencies of process from config
+                                                dependencies = Symbol.(processing_config.processors[process].dependencies)
+                                                # add all smaller ranks to list of dependencies and remove duplicates
+                                                dependencies = unique(vcat([processing_config.p_possible_process_steps[1:findfirst(processing_config.p_possible_process_steps .== dep)] for dep in dependencies]...))
+                                                # check if dependencies are met
+                                                if !all([p_process_status[period][dep] for dep in dependencies])
+                                                    @warn "Dependencies not yet met for $process"
+                                                end
+                                                # if not met wait till met
+                                                while !all([p_process_status[period][dep] for dep in dependencies])
+                                                    sleep(10)
+                                                end
+                                            end
+
+                                            # run process
+                                            kwargs = NamedTuple([(k, v) for (k, v) in pairs(processing_config.processors[process].kwargs)])
+                                            getfield(Main, process)(processing_config, l200, period, run,; kwargs...)
+                                            flush(stdout)
+
+                                            # update process status
+                                            process_status[period][process][run] = true
+                                        end
+                                    end
+                                end
+                            end
+                        end
+                    end
+
+                    # process partitions 
+                    Threads.@spawn begin
+                        for period in periods
+
+                            @info "Process partitions in $period"
+
+                            Threads.@spawn begin
+                                # iterate through process steps
+                                for process in p_process_steps
+                                    @info "$process"
+
+                                    # check if p process has dependencies
+                                    dependencies = Symbol.(processing_config.p_processors[process].dependencies)
+                                    # add all smaller ranks to list of dependencies and remove duplicates
+                                    dependencies = unique(vcat([processing_config.possible_process_steps[1:findfirst(processing_config.possible_process_steps .== dep)] for dep in dependencies]...))
+                                    if !all([all(values(process_status[period][dep])) for dep in dependencies])
+                                        @warn "Dependencies not yet met for $process"
+                                    end
+                                    while !all([all(values(process_status[period][dep])) for dep in dependencies])
+                                        sleep(10)
+                                    end
+                                    
+                                    # run process
+                                    kwargs = NamedTuple([(k, v) for (k, v) in pairs(processing_config.p_processors[process].kwargs)])
+                                    # make sure that only the first period in each partitions are processed
+                                    merge!(kwargs, (only_first_period = DataPeriod(period.no - 1) in periods, ))
+                                    # process partitions
+                                    getfield(Main, process)(processing_config, l200, period,; kwargs...)
+                                end
+                            end
                         end
                     end
                 end
             end
         end
     end
-end
+    # execute interactive menu
+    menu()
+else
+    @sync begin
 
-####################
-# Process Partitions
-####################
-if !processing_config.only_runs
+        ####################
+        # Process Runs
+        ####################
+        if !processing_config.only_partitions
+            
+            # get processing steps from config and sort by rank
+            process_steps =  processing_config.process_steps
 
-    # get processing steps from config for partitions
-    process_steps =  processing_config.p_process_steps
-
-    # get processable partitions
-    processable_partitions = get_processable_partitions(partitions)
-
-    # process partitions 
-    @sync begin 
-        for part in partitions
-
-            @info "Process partition $part"
-
-            # submit slurm job if enabled
-            if processing_config.submit_slurm
-                sbatch(l200, processing_config, part)
-                continue
-            end
-
+            # process periods
             Threads.@spawn begin
-                # iterate through process steps
-                for process in process_steps
-                    @info "$process"
+                for period in periods
+                    Threads.@spawn begin
+                        @info "Process period $period"
 
-                    # run process
-                    kwargs = NamedTuple([(k, v) for (k, v) in pairs(processing_config.p_processors[process]) if !(k in [:enabled, :rank, :n_workers])])
-                    getfield(Main, process)(processing_config, l200, part,; kwargs...)
+                        # select runs to process
+                        processable_runs = sort(collect(keys(last(first(process_status[period])))))
+
+                        # process runs
+                        for run in processable_runs
+                            # check if run is a analysis run if switched on
+                            if processing_config.analysis_runs_only && !is_lrun(l200, (period, run, :cal))
+                                @warn "Run $run is not a analysis run"
+                                continue
+                            end
+
+                            Threads.@spawn begin
+                                # iterate through process steps
+                                @info "Process run $run"
+                                for process in process_steps
+                                    @info "$process"
+                                    flush(stdout)
+                                    
+                                    # check if process has dependencies
+                                    if !processing_config.only_runs
+                                        # get all dependencies of process from config
+                                        dependencies = Symbol.(processing_config.processors[process].dependencies)
+                                        # add all smaller ranks to list of dependencies and remove duplicates
+                                        dependencies = unique(vcat([processing_config.p_possible_process_steps[1:findfirst(processing_config.p_possible_process_steps .== dep)] for dep in dependencies]...))
+                                        # check if dependencies are met
+                                        if !all([p_process_status[period][dep] for dep in dependencies])
+                                            @warn "Dependencies not yet met for $process"
+                                        end
+                                        # if not met wait till met
+                                        while !all([p_process_status[period][dep] for dep in dependencies])
+                                            sleep(10)
+                                        end
+                                    end
+
+                                    # run process
+                                    kwargs = NamedTuple([(k, v) for (k, v) in pairs(processing_config.processors[process].kwargs)])
+                                    getfield(Main, process)(processing_config, l200, period, run,; kwargs...)
+                                    flush(stdout)
+
+                                    # update process status
+                                    process_status[period][process][run] = true
+                                end
+                            end
+                        end
+                    end
+                end
+            end
+        end
+
+        ####################
+        # Process Partitions
+        ####################
+        if !processing_config.only_runs
+
+            # get processing steps from config for partitions
+            p_process_steps =  processing_config.p_process_steps
+
+            # process partitions 
+            Threads.@spawn begin
+                for period in periods
+
+                    @info "Process partitions in $period"
+
+                    Threads.@spawn begin
+                        # iterate through process steps
+                        for process in p_process_steps
+                            @info "$process"
+
+                            if !processing_config.only_partitions
+                                # check if p process has dependencies
+                                dependencies = Symbol.(processing_config.p_processors[process].dependencies)
+                                # add all smaller ranks to list of dependencies and remove duplicates
+                                dependencies = unique(vcat([processing_config.possible_process_steps[1:findfirst(processing_config.possible_process_steps .== dep)] for dep in dependencies]...))
+                                if !all([all(values(process_status[period][dep])) for dep in dependencies])
+                                    @warn "Dependencies not yet met for $process"
+                                end
+                                while !all([all(values(process_status[period][dep])) for dep in dependencies])
+                                    sleep(10)
+                                end
+                            end
+
+                            # run process
+                            kwargs = NamedTuple([(k, v) for (k, v) in pairs(processing_config.p_processors[process].kwargs)])
+                            # make sure that only the first period in each partitions are processed
+                            merge!(kwargs, (only_first_period = DataPeriod(period.no - 1) in periods, ))
+                            # process partitions
+                            getfield(Main, process)(processing_config, l200, period,; kwargs...)
+                        end
+                    end
                 end
             end
         end
     end
+    @info "# Processing Done"
 end
-
-@info "# Processing Done"
