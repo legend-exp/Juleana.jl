acdbfd8903e9ae8298f0309a9ecb6325219faaba
Florian Henkes <florian.henkes@outlook.com>
2024-11-14 11:23:18 -0500

    Fix reprocess log tuple

--- processors/process_sipm_thresholds.jl
+++ processors/process_sipm_thresholds.jl
@@ -38,7 +38,7 @@ function process_sipm_thresholds(processing_config::PropDict, l200::LegendData, 
 
         if !reprocess && haskey(pars_db, det)
             @debug "Channel $det already processed, skip"
-            log_ch = log_nt((ch, det, ProcessStatus(1), pars_db[det].τ, pars_db[det].n_tau, "Already processed --> skipped."))
+            log_ch = log_nt((ch, det, ProcessStatus(1), pars_db[det].trig.σ, pars_db[det].dc.σ, "Already processed --> skipped."))
             return (processed = false, log = log_ch)
         end
 
