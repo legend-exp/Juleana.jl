d36e35818034818dd760e4fb59c9ca2919ba0ee4
Florian Henkes <florian.henkes@outlook.com>
2025-03-22 23:16:30 +0100

    Only delete PMT files if they exist

--- processors/process_evt_phy.jl
+++ processors/process_evt_phy.jl
@@ -35,7 +35,12 @@ function process_evt_phy(processing_config::PropDict, l200::LegendData, period::
                     @info "Reprocess $(basename(evtfilename)), remove old Evt."
                     rm(outfilename, force=true)
                     rm(evtfilename, force=true)
-                    rm(pmtoutfilename, force=true)
+                    if isfile(pmtoutfilename)
+                        rm(pmtoutfilename, force=true)
+                    end
+                    if isfile(pmtevtfilename)
+                        rm(pmtevtfilename, force=true)
+                    end
                 elseif isfile(outfilename)
                     @info "File $(basename(evtfilename)) already exists, skip"
                     n_forced, n_pulser, n_phy = lh5open(outfilename, "r") do ds
