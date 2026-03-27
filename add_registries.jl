9e9dc6e3fb4fe6c4aaed9fdc4ea4f913ee80a25e
jschlegel <jonas.schlegel@mpi-hd.mpg.de>
2026-03-27 15:19:23 +0100

    Hotfix cache

--- add_registries.jl
+++ add_registries.jl
@@ -1,2 +1,9 @@
 using Pkg
+
+# Remove stale LegendJuliaRegistry cache to avoid rebase failures on CI
+legend_reg = joinpath(first(DEPOT_PATH), "registries", "LegendJuliaRegistry")
+if isdir(legend_reg)
+    rm(legend_reg; recursive=true)
+end
+
 pkg"registry add General https://github.com/legend-exp/LegendJuliaRegistry.git"
