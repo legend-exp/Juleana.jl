c2bbc424dae236afe84fcb9185340b65adff6a76
Andreas Gieb <andreas.gieb@mpi-hd.mpg.de>
2026-09-16 17:06:32 +0200

    use new read_ldata functionality to load only the data needed for processing

--- processors/p_process_energy.jl
+++ processors/p_process_energy.jl
@@ -96,7 +96,7 @@ function p_process_energy(processing_config::PropDict, l200::LegendData, period:
                 try
                     energy = fast_flatten([begin 
                             @debug "Reading from $(pinfo.period)-$(pinfo.run)"
-                            ljl_propfunc(l200.par.rpars.ecal[pinfo.period, pinfo.run][det][e_type].cal.func).(read_ldata(l200, :jldsp, :cal, pinfo.period, pinfo.run, det; filterby = :jlqcs => @pf($is_single_pulse && !$is_pulser))) end
+                            read_ldata(ljl_propfunc(l200.par.rpars.ecal[pinfo.period, pinfo.run][det][e_type].cal.func), l200, :jldsp, :cal, pinfo.period, pinfo.run, det; filterby = :jlqcs => @pf($is_single_pulse && !$is_pulser)) end
                     for pinfo in partinfo_det])
                 catch e
                     @error "E data for $det from cannot be loaded"
